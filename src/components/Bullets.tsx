27abf3adb0cfde02d7e5f51abbea4d96f9d3a95e
pedrocsencon <pedro.copatti@sencon.com.br>
2022-08-28 12:25:22 -0300

    refactor

--- src/components/Bullets.tsx
+++ src/components/Bullets.tsx
@@ -1,4 +1,4 @@
-import { Divider, Flex, Heading, Stack } from "@chakra-ui/react";
+import { Divider, Flex, Heading, Stack, useMediaQuery } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import { BsPatchQuestionFill } from "react-icons/bs";
 import { FaChalkboardTeacher, FaUsers } from "react-icons/fa";
@@ -7,21 +7,49 @@ import { ImPriceTag } from "react-icons/im";
 import BulletItem from "./BulletItem";
 
 export default function Bullets() {
-              const MFlex = motion(Flex);
+  const MFlex = motion(Flex);
+  const [isLargerThan1200] = useMediaQuery("(min-width: 1200px)");
 
-            return (
-                        <Stack shadow='xl' backgroundColor='white.50' pl='8' borderRadius='32' mt='32' w='90%' direction='row' spacing='4'>
-                                    <Stack direction='row' align='center' spacing='8'>
-                                                <MFlex animate={{ rotate: [0, -15, 15, 0] }} transition={{ repeat: Infinity, duration: 2 }}><BsPatchQuestionFill size={56} color='#4EB7CD' /></MFlex>
-                                                <Heading size='md' w='60%'>Até quando você irá postergar o início dos seus estudos em francês?</Heading>
-                                                <Divider orientation='vertical' h='12' />
-                                    </Stack>
-                                    <Flex backgroundColor='orange.500' p='12' borderRadius='32' justify='space-between' direction='row' align='baseline' width='100%'>
-                                                <BulletItem icon={FaChalkboardTeacher} text="Professor nativo de frança" />
-                                                <BulletItem icon={FaUsers} text="Turmas de no máx. 5 pessoas" />
-                                                <BulletItem icon={HiSupport} text="Atenção individualizada" />
-                                                <BulletItem icon={ImPriceTag} text="Valor Promocional" />
-                                    </Flex>
-                        </Stack>
-            )
+  return (
+    <Stack
+      shadow="xl"
+      backgroundColor="white.50"
+      pl="8"
+      borderRadius="32"
+      mt="32"
+      w="90%"
+      direction={isLargerThan1200 ? "row" : "column"}
+      spacing="4"
+    >
+      <Stack direction="row" align="center" spacing="8">
+        <MFlex
+          animate={{ rotate: [0, -15, 15, 0] }}
+          transition={{ repeat: Infinity, duration: 2 }}
+        >
+          <BsPatchQuestionFill size={56} color="#4EB7CD" />
+        </MFlex>
+        <Heading size="md" w="60%">
+          Até quando você irá postergar o início dos seus estudos em francês?
+        </Heading>
+        {isLargerThan1200 ? <Divider orientation="vertical" h="12" /> : <></>}
+      </Stack>
+      <Flex
+        backgroundColor="orange.500"
+        p="12"
+        borderRadius="32"
+        justify="space-between"
+        direction="row"
+        align="baseline"
+        width="100%"
+      >
+        <BulletItem
+          icon={FaChalkboardTeacher}
+          text="Professor nativo de frança"
+        />
+        <BulletItem icon={FaUsers} text="Turmas de no máx. 5 pessoas" />
+        <BulletItem icon={HiSupport} text="Atenção individualizada" />
+        <BulletItem icon={ImPriceTag} text="Valor Promocional" />
+      </Flex>
+    </Stack>
+  );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,70 @@
-import { Badge, Button, Heading, Stack, Text } from "@chakra-ui/react"
-import { motion } from "framer-motion"
-import Image from "next/image"
-import illustration from '../assets/illustration.svg'
+import {
+  Badge,
+  Button,
+  Heading,
+  Stack,
+  Text,
+  useMediaQuery,
+} from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { Link } from "react-scroll";
+import illustration from "../assets/illustration.svg";
 
 export default function Header() {
-            const MBadge = motion(Badge);
-            return (
-                        <Stack mt='24' w='90%' h='40vh' justify='center' direction='row' align='center'>
-                                    <Stack w='60%' justify='center' spacing='8' >
-                                                <MBadge animate={{scale: [1, 1.1, 1]}} transition={{ repeat: Infinity, duration: 1 }} fontSize='1rem' colorScheme='orange' variant='outline' maxWidth='95px'>NOVIDADE</MBadge>
-                                                <Heading color='black.500' size='2xl'>Aprenda francês em 3 meses com professor nativo!</Heading>
-                                                <Text color='black.50'>Professor nativo se junta a doutora em francês para dar curso iniciante da língua francesa com turmas EXCLUSIVAS.</Text>
-                                                <Stack direction='row' spacing='8'>
-                                                            <Button height='50px' variant='solid' background='orange.500' color='white' _hover={{ backgroundColor: 'orange.900' }}>Quero começar</Button>
-                                                            <Button height='50px' variant='outline' >Saiba mais</Button>
-                                                </Stack>
-                                                <Text fontSize='12' color='orange.500'>Últimas vagas para nossas turmas de francês.</Text>
+  const [isLargerThan1200] = useMediaQuery("(min-width: 1200px)");
 
-                                    </Stack>
-                                    <Image alt='illustration' width='750px' src={illustration} />
-                        </Stack>
-            )
+  const MBadge = motion(Badge);
+  return (
+    <Stack
+      mt="24"
+      w="90%"
+      h="40vh"
+      justify="center"
+      direction={isLargerThan1200 ? "row" : "column"}
+      align="center"
+    >
+      <Stack w={isLargerThan1200 ? "60%" : "100%"} justify="center" spacing="8">
+        <MBadge
+          animate={{ scale: [1, 1.1, 1] }}
+          transition={{ repeat: Infinity, duration: 1 }}
+          fontSize="1rem"
+          colorScheme="orange"
+          variant="outline"
+          maxWidth="95px"
+        >
+          NOVIDADE
+        </MBadge>
+        <Heading color="black.500" size="2xl">
+          Aprenda francês em 3 meses com professor nativo!
+        </Heading>
+        <Text color="black.50">
+          Professor nativo se junta a doutora em francês para dar curso
+          iniciante da língua francesa com turmas EXCLUSIVAS.
+        </Text>
+        <Stack direction="row" spacing="8">
+          <Link to="planos" smooth offset={-24}>
+            <Button
+              height="50px"
+              variant="solid"
+              background="orange.500"
+              color="white"
+              _hover={{ backgroundColor: "orange.900" }}
+            >
+              Quero começar
+            </Button>
+          </Link>
+          <Link to="bgImage" smooth offset={-24}>
+            <Button height="50px" variant="outline">
+              Saiba mais
+            </Button>
+          </Link>
+        </Stack>
+        <Text fontSize="12" color="orange.500">
+          Últimas vagas para nossas turmas de francês.
+        </Text>
+      </Stack>
+      <Image alt="illustration" width="750px" src={illustration} />
+    </Stack>
+  );
 }
